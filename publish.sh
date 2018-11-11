d3a0e86b6a4d5b7cc43b5f6ff08d007004b5fda1
Paul Laffitte <paul.laffitte@epitech.eu>
2018-11-11 22:41:23 +0100

    publish develop on master

---
+++ publish.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+read -p "This action will hard reset master onto develop and force push. Are you sure? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Nn]$ ]];
+	then exit
+fi
+
+TMPDIR=$(mktemp -d)
+
+cp . $TMPDIR -R
+cd $TMPDIR
+
+git add --all
+git stash
+
+git checkout develop
+git fetch origin
+
+git checkout master
+git reset --hard origin/develop
+
+yarn
+yarn run build
+
+sed .gitignore -i -e 's/assets\/js\/\*\.js//'
+git add --all
+git commit -m "build"
+git push --force
+
+rm $TMPDIR -rf
