077abc39e83cb64b061345040aa7004e5628500c
Paul Laffitte <plaffitt@gmail.com>
2024-12-05 01:35:16 +0100

    build

--- src/components/Text.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Text = ({ text }) => {
-  const paragraphs = text.split('\n');
-  return paragraphs.map((text, i) => <p key={ i }>{ text }</p>);
-};
-
-export default Text;
